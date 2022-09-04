74d488ea87001adbceadcda595bf32b415f29d56
Semen <semeongluhov@yandex.ru>
2022-09-04 20:41:25 +0300

    Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -7,7 +7,7 @@
 Console.Clear();
 Console.Write("Введите трёхзначное целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number >= 100 && number <= 1000)
+if (number >= 100 && number < 1000)
 {
     int MaxDigit(int num)
     {
