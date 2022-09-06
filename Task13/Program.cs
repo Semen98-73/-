02452c882ba8a295925a22db80f9d1dfaf533f16
Semen <semeongluhov@yandex.ru>
2022-09-06 19:02:27 +0300

    Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,3 +3,26 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); ;
+int num = number;
+if (number <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (number >= 100 && number < 1000)
+{
+    num = number % 10;
+    Console.WriteLine($"{num}");
+}
+else
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine($"{num}");
+}
