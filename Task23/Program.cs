6f0cabe0b4671f4ff283e1f1068aafcfa9a3e50b
Semen <semeongluhov@yandex.ru>
2022-09-07 18:16:24 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (digit > 0)
+{
+    while (count <= digit)
+    {
+        Console.WriteLine($"{count} -> {count * count * count}");
+        count++;
+    }
+}
+else if (digit <= 0)
+{
+    while (count >= digit)
+    {
+        Console.WriteLine($"{count} -> {count * count * count}");
+        count--;
+    }
+}
+
